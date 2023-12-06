aa1665ceeca1808dabfe72974af58daeea7cd400
Luis Fernando Saboia <135769669+luisflsj@users.noreply.github.com>
2023-12-06 16:28:56 -0300

    Update database.py

--- database.py
+++ database.py
@@ -1,4 +1,4 @@
 import pandas as pd
 import streamlit as st
 
-df_fortcasa = pd.read_csv('dados/relatorio_fortcasa.csv', delimiter= ';')
+df_fortcasa = pd.read_csv('relatorio_fortcasa.csv', delimiter= ';')
